f678acdcca604eb79537aa41512b20f4dded2d65
Fahid Javid <fahidjavid@gmail.com>
2017-08-08 09:41:40 +0500

    Show all & Hide All app.

---
+++ Show All & Hide All/Show All & Hide All/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Show All & Hide All
+//
+//  Created by Fahid Javid on 05/07/2017.
+//  Copyright © 2017 Fahid Javid. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Show All & Hide All/Show All & Hide All/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Show All & Hide All
+//
+//  Created by Fahid Javid on 05/07/2017.
+//  Copyright © 2017 Fahid Javid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
