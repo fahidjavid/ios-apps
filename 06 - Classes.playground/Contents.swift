1e98e90153979be908f5c0e5362f4278a7df6b6f
Fahid Javid <fahidjavid@gmail.com>
2017-08-09 08:02:17 +0500

    06 - Classes

---
+++ 06 - Classes.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+class User {
+
+    var name = ""
+    var age = 0
+    var FavoriteMove = ""
+    var FavoriteWeather = ""
+    var isDeveloper = false
+}
+
+
+var newUser = User()
+
+newUser.name = "Fahid Javid"
+newUser.age = 25
+newUser.FavoriteWeather = "Cloudy"
+newUser.isDeveloper = true
+
