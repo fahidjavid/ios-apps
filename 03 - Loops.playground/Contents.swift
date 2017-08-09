fbe2309e7014440464c0f6e2af22b4cfca20d186
Fahid Javid <fahidjavid@gmail.com>
2017-08-09 07:42:50 +0500

    03 - Loops

---
+++ 03 - Loops.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myNumber = 1
+
+// while loop
+while myNumber < 10 {
+    
+    myNumber += 1
+    
+    print(myNumber)
+}
+
+var myAnimalArray = ["Lion", "Crab", "Hippo", "Parrot"]
+
+
+// for loop
+for animal in myAnimalArray {
+    print(animal)
+}
