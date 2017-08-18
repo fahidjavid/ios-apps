5fb393146f4287fd3955398ede5338edbd7992c7
Fahid Javid <fahidjavid@gmail.com>
2017-08-18 06:55:00 +0500

    Project Layout

---
+++ Project Layout/Project Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project Layout
+//
+//  Created by Fahid Javid on 18/08/2017.
+//  Copyright © 2017 Fahid Javid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
