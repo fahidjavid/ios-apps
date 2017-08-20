ba2e92b5e81e71d3a0de0dbd65108b8014aa3071
Fahid Javid <fahidjavid@gmail.com>
2017-08-20 14:38:46 +0500

    Alert Views - Complete
    The previous related commit has some missing files issues. So this one is correct and complete.

--- Alert Views/Alert Views/ViewController.swift
+++ Alert Views/Alert Views/ViewController.swift
@@ -9,24 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var passwordAgainText: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-    @IBAction func myButtonClicked(_ sender: Any) {
+    @IBAction func signupBtnClicked(_ sender: Any) {
         
-        let alert = UIAlertController(title: "Error", message: "Very critical error, such wow", preferredStyle: UIAlertControllerStyle.alert)
-        
-        let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
-        
-        alert.addAction(okButton)
-        
-        self.present(alert, animated: true, completion: nil)
+        if userNameText.text == "" {
+            
+            let alert = UIAlertController(title: "Error", message: "Please provide a username", preferredStyle: UIAlertControllerStyle.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        } else if passwordText.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Please provide a password", preferredStyle: UIAlertControllerStyle.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        } else if passwordText.text != passwordAgainText.text {
+            let alert = UIAlertController(title: "Error", message: "Password didn't match", preferredStyle: UIAlertControllerStyle.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Success", message: "\(userNameText.text) is created", preferredStyle: UIAlertControllerStyle.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
-    
-    
 }
 
